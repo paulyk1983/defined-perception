0fb4de70959dfe218244bef1e54dbf6decc6aeaf
Paul <paulyk1983@gmail.com>
2017-03-25 18:56:26 -0500

    added tracks page and user sign in logic

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,10 @@
+class TracksController < ApplicationController
+  def index
+    if current_user
+      render 'index.html.erb'
+    else
+      flash[:error] = "You must be logged in to view that page"
+      redirect_to '/'
+    end
+  end
+end
